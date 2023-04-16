c80fd88d56e46fd6bbb5a0746d2071696231ed32
Sol/Dreadwyrm <dreadwyrm@Azure-in-Monochrome>
2023-04-16 17:49:12 -0500

    Quiz Complete

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,9 +1,12 @@
 # What is the data type in the following variable?
-my_var = {"name": "John", "age": 23} # answer here
+my_var = {"name": "John", "age": 23} #This would be a tuple? An unchangeable data set
 
 # Write a loop that prints out numbers from 1 to 10.
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(digits)
 
 # Write a function that returns a sentence using a name given to it.
+print("This is a print function in a sentence.")
 # Remember to call the function so that it prints in the console!
 
 # BONUS: What are the four different types of collections in python?
